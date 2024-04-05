56a343efafc5c172d504cfa68b9fedfca983a829
brunahenz <brunaehenz@gmail.com>
2024-04-05 11:57:25 -0300

    colocando os links no app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
-import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/marca">Marcas</router-link> |
+    <router-link to="/categoria">Categorias</router-link> |
+    <router-link to="/acessorio">Acessorios</router-link> |
+    <router-link to="/cor">Cores</router-link> |
   </header>
   <main>
     <RouterView />
