87f9e1d11ba7cfb2ff06bf182193254b4a10fb6a
brunahenz <brunaehenz@gmail.com>
2024-04-05 11:57:58 -0300

    criação das rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import CoresView from "../views/CoresView.vue";
+import MarcasView from "../views/MarcasView.vue";
+import AcessoriosView from "../views/AcessoriosView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +14,25 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/categorias",
-      name: "categorias",
+      path: "/categoria",
+      name: "categoria",
       component: CategoriaView,
     },
+    {
+      path: "/acessorio",
+      name: "acessorio",
+      component: AcessoriosView,
+    },
+    {
+      path: "/marca",
+      name: "marca",
+      component: MarcasView,
+    },
+    {
+      path: "/cor",
+      name: "cor",
+      component: CoresView,
+    }
   ],
 });
 
